70df7e040082b37ee9198ed8f5c49d2e46c82dd9
hoang <hoangdn362@gmail.com>
2022-11-22 13:19:41 +0700

    .

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,39 +1,40 @@
 import React from "react";
 import Post from "./Post";
 import "./Home.css";
-import Footer from "../components/Home/Footer"
-import {Route,Routes,Navigate} from "react-router-dom";
+import Footer from "../components/Home/Footer";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "../components/Navbar";
 import Recruitment from "./Recruitment";
-import NotFound from '../utils/notFound';
+import NotFound from "../utils/notFound";
 import { Home } from "./Home";
 import MyJobPost from "./MyJobPost";
-import UpdateJob from "../components/Job/UpdateJob"
+import UpdateJob from "../components/Job/UpdateJob";
 import RecruitmentDetail from "../components/Job/JobDetail";
-import HomeProfile from "../components/ProfileStudent/HomeProfile"
+import HomeProfile from "../components/ProfileStudent/HomeProfile";
 import ListStudent from "./ListStudent";
-export default function Homepage(){
-    const auth = useSelector(state => state.auth)
-    const {isLogged} = auth;
-    return(
-        (!isLogged) ? (<Navigate to="/"/>) : (
-            <>
-            <Navbar/>
-            <Routes>
-                <Route path= "/"  element={<Home/> }/>
-                <Route path= "/post" element={<Post/>}/>
-                <Route path = "/recruitment/myjobpost" element={<MyJobPost/>}/>
-                <Route path= "/recruitment/newest" element={<Recruitment/> }/>
-                <Route path = "/recruitment/detail/:id" element={<RecruitmentDetail/>}/>
-                <Route path = "/recruitment/edit/:id" element={<UpdateJob/>}/>
-                <Route path= "/profile" exact element={<HomeProfile/>}/>
-                <Route path= "*"  element={<NotFound/> }/>
-                <Route path= "/liststudent"  element={<ListStudent/> }/>
-            </Routes>
-            <Footer/>
-             </> 
-        )  
-    )
-    ;
+import { CreatePost } from "./CreatePost";
+export default function Homepage() {
+  const auth = useSelector((state) => state.auth);
+  const { isLogged } = auth;
+  return !isLogged ? (
+    <Navigate to="/" />
+  ) : (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/recruitment/myjobpost" element={<MyJobPost />} />
+        <Route path="/recruitment/newest" element={<Recruitment />} />
+        <Route path="/recruitment/detail/:id" element={<RecruitmentDetail />} />
+        <Route path="/recruitment/edit/:id" element={<UpdateJob />} />
+        <Route path="/profile" exact element={<HomeProfile />} />
+        <Route path="/create-post" exact element={<CreatePost />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/liststudent" element={<ListStudent />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
--- src/pages/ListStudent.js
+++ src/pages/ListStudent.js
@@ -1,60 +1,66 @@
-import React,{useState} from 'react'
-import {Box, Grid, ThemeProvider, Typography, Button, Pagination} from "@mui/material"
-import theme from "../theme";
-import SearchBar from '../components/ListStudent/SearchBar2';
+import React, { useState } from "react";
+import {
+  Box,
+  Grid,
+  ThemeProvider,
+  Typography,
+  Button,
+  Pagination,
+} from "@mui/material";
+import theme from "../components/Job/theme";
+import SearchBar from "../components/ListStudent/SearchBar2";
 
-import JobData from "../components/ListStudent/dummyData2"
-import JobList from '../components/ListStudent/StudentList';
+import JobData from "../components/ListStudent/dummyData2";
+import JobList from "../components/ListStudent/StudentList";
 
-const ListStudent=() => {
+const ListStudent = () => {
   const [openPopup, setOpenPopup] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState("");
   const searchHandle = (searchTerm) => {
     setSearchTerm(searchTerm);
-    if(searchTerm !== "")
-    {
+    if (searchTerm !== "") {
       const newJobList = JobData.filter((job) => {
         return Object.values(job)
-        .join("")
-        .toLowerCase()
-        .includes(searchTerm.toLowerCase());
+          .join("")
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase());
       });
       setSearchResults(newJobList);
-    }
-    else{
+    } else {
       setSearchResults(JobData);
     }
-  }
+  };
   return (
     <>
-          <ThemeProvider theme={theme} >
-          
-              <Box bgcolor="white" py={2} sx={{marginTop:0,width:"100%"}}>
-                <Grid container justifyContent="center">  
-                  <Grid item xs={10}>
-                    <Box display="flex" justifyContent="space-between">
-                    <Typography variant='h3'>Student</Typography>
-                    {/*  */}
-                    </Box>
-                  </Grid>               
-                </Grid>
+      <ThemeProvider theme={theme}>
+        <Box bgcolor="white" py={2} sx={{ marginTop: 0, width: "100%" }}>
+          <Grid container justifyContent="center">
+            <Grid item xs={10}>
+              <Box display="flex" justifyContent="space-between">
+                <Typography variant="h3">Student</Typography>
+                {/*  */}
               </Box>
-              <Grid container justifyContent="center">
-                <Grid item xs={10}>
-                  <SearchBar term = {searchTerm}
-                    searchKeyWord = {searchHandle}
-                  />
-                  <JobList jobs={searchTerm.length < 1 ? JobData : searchResults}/>
-                </Grid>
-              </Grid>
-              <Pagination style={{marginTop:"40px"}} count={10} variant="outlined" shape="rounded" showFirstButton showLastButton  />
-              
-          </ThemeProvider> 
+            </Grid>
+          </Grid>
+        </Box>
+        <Grid container justifyContent="center">
+          <Grid item xs={10}>
+            <SearchBar term={searchTerm} searchKeyWord={searchHandle} />
+            <JobList jobs={searchTerm.length < 1 ? JobData : searchResults} />
+          </Grid>
+        </Grid>
+        <Pagination
+          style={{ marginTop: "40px" }}
+          count={10}
+          variant="outlined"
+          shape="rounded"
+          showFirstButton
+          showLastButton
+        />
+      </ThemeProvider>
     </>
-  )
-}
+  );
+};
 
-
-export default ListStudent
-
+export default ListStudent;
