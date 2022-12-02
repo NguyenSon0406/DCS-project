97db1f650303ae0e26efd350096ae121fa4417af
NguyenSon0406 <nguyenson062001@gmail.com>
2022-12-03 00:57:02 +0700

    fix ui post

--- src/components/Article/ArticlePost/CommentBox.js
+++ src/components/Article/ArticlePost/CommentBox.js
@@ -2,34 +2,37 @@ import { CommentSection } from "react-comments-section";
 import "react-comments-section/dist/index.css";
 import React from "react";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
-export default function App() {
+export default function CommentBox() {
+  const auth = useSelector(state => state.auth)
+  const {user} = auth;
+
   const [data] = useState([]);
 
   return (
     <div className="CommentBox">
       <CommentSection
         customNoComment={() => {
-          return <div>Không có bình luận</div>;
+          return <div>No comments</div>;
         }}
         currentUser={{
           currentUserId: "01a",
-          currentUserImg:
-            "https://ui-avatars.com/api/name=Riya&background=random",
+          currentUserImg: user.avatar,
           currentUserProfile:
             "https://www.linkedin.com/in/riya-negi-8879631a9/",
           currentUserFullName: "Nguyễn Sơn"
         }}
         advancedInput={true}
-        hrStyle={{ border: "0.5px solid #ff0072" }}
+        hrStyle={{ border: "0.5px solid #ff0072",width:"auto", margin: 0 }}
         commentData={data}
         logIn={{
           loginLink: "http://localhost:3001/",
           signupLink: "http://localhost:3001/"
         }}
-        customImg="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F13%2F2015%2F04%2F05%2Ffeatured.jpg&q=60"
+        customImg={user.avatar}
         inputStyle={{ border: "1px solid rgb(208 208 208)" }}
-        formStyle={{ backgroundColor: "white" }}
+        formStyle={{ backgroundColor: "white",width:"auto", margin: 0 }}
         submitBtnStyle={{
           border: "1px solid black",
           backgroundColor: "black",
